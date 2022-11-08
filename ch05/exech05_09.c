ee1878272574a8d21a282ddb40b1bf6e5a0c4161
LIUzhihao <xiao.zhi.l@qq.com>
2022-11-08 09:47:19 +0800

    修改第5章09练习

--- ch05/exech05_09.c
+++ ch05/exech05_09.c
@@ -9,27 +9,29 @@ int main(void)
     double db_T_F;
     int quit_flag;
 
-    printf("This program computes moduli.\n");
-    printf("Enter an integer to save as the second operad: ");
+    printf("Enter temperature number in fahrenheit: ");
     quit_flag = scanf("%lf",&db_T_F);
 
     while(quit_flag == 1)
     {
         Temperatures(db_T_F);
-        printf("Enter the next number for first operatd(<= 0 to quit): ");
+        printf("Enter the next temperature number in fahrenheit(nonnumeric character to quit): ");
         quit_flag = scanf("%lf",&db_T_F);
     }
-    printf("Done\n");
+    printf("Bye.\n");
 
     return 0;
 }
 
 void Temperatures(double db_T_F)
 {
-    const float F_TO_C = 1;
-    const float C_TO_K = 2;
+    const float F_TO_C_k = 5.0/9.0, T_TO_C_b = 32.0;
+    const float C_TO_K_b = 237.16;
     double db_T_C,db_T_K;
 
-    /* [ TODO ] */
-    //printf("%d %% %d is %d. \n",num_first,num_second,num_first % num_second);
+    db_T_C = F_TO_C_k * (db_T_F - T_TO_C_b);
+    db_T_K = db_T_C + C_TO_K_b;
+
+    printf("%.2f F = %.2f C = %.2f K. \n",db_T_F,db_T_C,db_T_K);
+
 }
